d6da8448ed6917179c7db67e14adec5e5d5d14b4
Stephania <stephania.guzman@pucp.edu.pe>
2022-04-19 09:35:50 -0500

    mi primer commit

---
+++ src/main/java/com/example/pruebalab3/Pruebalab3Application.java
@@ -0,0 +1,13 @@
+package com.example.pruebalab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pruebalab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pruebalab3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pruebalab3/Pruebalab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebalab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pruebalab3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
