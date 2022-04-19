6895fc708e352a7b026b83f74adf56c3f44418c8
Stephania <stephania.guzman@pucp.edu.pe>
2022-04-19 12:13:48 -0500

    Entity x2

--- src/main/java/com/example/pruebalab3/Entity/Employees.java
+++ src/main/java/com/example/pruebalab3/Entity/Employees.java
@@ -128,6 +128,7 @@ public class Employees {
     public void setManagerid(Employees managerid) {
         this.managerid = managerid;
     }
+
     public Departments getDepartmentid() {
         return departmentid;
     }
